7a153abd30c84ef62313e462a0d6fcb5da38cd20
McKenzie Macdonald <kenziemacd@icloud.com>
2022-01-12 14:29:35 -0800

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by McKenzie Macdonald on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
